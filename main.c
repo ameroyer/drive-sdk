f513d7b3a05bd0b50157aac9d77dce1c5e0af613
ameroyer <ameroyer@users.noreply.github.com>
2015-10-16 11:15:59 +0200

    Adding code draft

---
+++ main.c
@@ -0,0 +1,9 @@
+/*
+ * Main File for controlling the car vehicle
+ * Use scripts in the commands folder (simpler version of vehicle tools) and in the FlyExamples forlder (get image from shared memory)
+ */
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
