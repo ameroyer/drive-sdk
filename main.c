bcf2f7da74bf807e0dd3aee42c6335783f1ce936
ameroyer <ameroyer@users.noreply.github.com>
2015-10-18 12:13:23 +0200

    adding simpler display

--- main.c
+++ main.c
@@ -8,12 +8,26 @@
 
 /***
 List of command calls
+* cmd_connect
 * cmd_anki_vehicle_read_simple(1, char* name)
 
 
 * cmd_anki_vehicle_disconnect_simple()
-* Note to check: some command are already defined in non static somewher, eg cmnd_anki_vehicle_sdk_mode
+* cmd_anki_vehicle_sdk_mode_simple (int mode)
+* cmd_anki_vehicle_ping_simple()
+
+* cmd_anki_vehicle_uturn_simple()
+* cmd_anki_vehicle_get_version_simple()
+* cmd_anki_vehicle_get_localization_position_update_simple()
+* cmd_anki_vehicle_set_speed_simple(int speed_, int accel_) (default accel = 25000) 
+* cmd_anki_vehicle_change_lane_simple(int hspeed_, int haccel_, float offset) default offset = 1
+* cmd_anki_vehicle_goto_lane_simple(int hspeed_, int haccel_, float offset) default offset = 1
 *///
+
+///CHANGE LANESET SPEED:                 rl_printf("Usage: %s <horizontal speed (mm/sec)> <horizontal accel (mm/sec^2)> <offset (mm)>\n", argvp[0]);
+
+////GOTOLLANE        if (argcp < 3) {
+//       rl_printf("Usage: %s <horizontal speed (mm/sec)> <horizontal accel (mm/sec^2)> <offset from center (mm)>\n", argvp[0]);
 
 //TODO: MAC connection
 int main(int argc, char *argv[])
