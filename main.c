18c49fbcdea9acb82b3d83badf3450011bb27a9d
ameroyer <ameroyer@users.noreply.github.com>
2015-10-18 15:40:26 +0200

    Adding header file + clean

--- main.c
+++ main.c
@@ -4,35 +4,18 @@
  */
 
 
-//#include "examples/vehicle-tool/vehicle_cmd_simple.c"
-//TODO header file for cmd8simple
-/***
-    List of command calls
-    * cmd_connect
-    * cmd_anki_vehicle_read_simple(1, char* name)
+#include "examples/vehicle-tool/vehicle_cmd_simple.h"
 
-
-    * cmd_anki_vehicle_disconnect_simple()
-    * cmd_anki_vehicle_sdk_mode_simple (int mode)
-    * cmd_anki_vehicle_ping_simple()
-
-    * cmd_anki_vehicle_uturn_simple()
-    * cmd_anki_vehicle_get_version_simple()
-    * cmd_anki_vehicle_get_localization_position_update_simple()
-    * cmd_anki_vehicle_set_speed_simple(int speed_, int accel_) (default accel = 25000) 
-    * cmd_anki_vehicle_change_lane_simple(int hspeed_, int haccel_, float offset) default offset = 1
-    * cmd_anki_vehicle_goto_lane_simple(int hspeed_, int haccel_, float offset) default offset = 1
-
-///CHANGE LANESET SPEED:                 rl_printf("Usage: %s <horizo//ntal speed (mm/sec)> <horizontal accel (mm/sec^2)> <offset (mm)>\n"// , argvp[0])
-
-////GOTOLLANE        if (argcp < 3) {
-//       rl_printf("Usage: %s <horizontal speed (mm/sec)> <horizontal// accel (mm/sec^2)> <offset from center (mm)>\n", argvp[0]);
-
-//TODO: MAC connection
-*/
+// boson D9:81:41:5C:D4:31
+// katal D8:64:85:29:01:C0
+// kourai EB:0D:D8:05:CA:1A
+// rho E6:D8:52:F1:D9:43
 
 int main(int argc, char *argv[]) {
+  cmd_connect_simple("D8:64:85:29:01:C0", "random");
   cmd_anki_vehicle_sdk_mode_simple(1);
+  cmd_anki_vehicle_set_speed_simple(500, 25000);
+  cmd_anki_vehicle_disconnect_simple();
   //cmd_help(1, 0);
   return 0;
 }
