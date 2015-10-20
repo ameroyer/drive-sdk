079637339e9961b2a2866638f3e8abd957ae76ea
ameroyer <ameroyer@users.noreply.github.com>
2015-10-20 10:22:28 +0200

    Cleaning main.c

--- main.c
+++ main.c
@@ -44,43 +44,40 @@ void print_loc(AnkiHandle h){
  * Main routine
  **/
 int main(int argc, char *argv[]) {
-// Example: Take picture
-GrabImagesFromSharedMemory(1);  
+   // Example: Take picture
+   GrabImagesFromSharedMemory(1);  
 
-// Read parameters
-if(argc<2){
-fprintf(stderr, "usage: %s car-name [adaptater] [verbose]\n",argv[0]);
-exit(0);
-}
-const char* adapter = "hci0";
-const char* car_id  = get_car_mac(argv[2]);
-if (argc > 2) {
-adapter = argv[2];
-}
+    // Read parameters
+    if(argc<2){
+	fprintf(stderr, "usage: %s car-name [adaptater] [verbose]\n",argv[0]);
+	exit(0);
+    }
+    const char* adapter = "hci0";
+    const char* car_id  = get_car_mac(argv[2]);
+    if (argc > 2) {
+	adapter = argv[2];
+    }
 
-// Init bluethooth and wait for connection successful
-AnkiHandle h = anki_s_init(adapter, car_id, argc>3);
-printf( anki_s_is_connected(h));
-while(!anki_s_is_connected(h)) {};
+    // Init bluethooth and wait for connection successful
+    fprintf(stderr, "Attempt connection\n");
+    AnkiHandle h = anki_s_init(adapter, car_id, argc>3);
+    while(!anki_s_is_connected(h)) {};
+    fprintf(stderr, "Connection successful\n");
 
-int i;
-if(anki_s_set_speed(h,1000,20000)!=0) return 1;
-//for(i=0; i<10; i++){ usleep(200000); anki_s_change_lane(h,-40,100,1000); }  
-for(i=0; i<10; i++){ usleep(200000);  print_loc(h);  }
-anki_s_set_speed(h,500,20000);
-for(i=0; i<10; i++){ usleep(200000);  print_loc(h);  }
-anki_s_change_lane(h,-50,100,1000);
-for(i=0; i<10; i++){ usleep(200000);  print_loc(h);  }
-anki_s_set_speed(h,0,20000);
-sleep(1);
-anki_s_close(h);
-sleep(2);
-AnkiHandle h2 = anki_s_init(adapter, car_id, argc>3);  
-usleep(500000);
-if(anki_s_set_speed(h2,1000,20000)!=0) return 1;
-for(i=0; i<5; i++){ usleep(200000);  print_loc(h);  }
-anki_s_set_speed(h2,0,20000);
-usleep(200000);
-anki_s_close(h2);
-return 0;
+    // Send commands
+    int i;
+    if(anki_s_set_speed(h,1000,20000)!=0) return 1;
+    print_loc(h);
+    //for(i=0; i<10; i++){ usleep(200000); anki_s_change_lane(h,-40,100,1000); }  
+    for(i=0; i<10; i++){ usleep(200000);  print_loc(h);  }
+    anki_s_set_speed(h,500,20000);
+    for(i=0; i<10; i++){ usleep(200000);  print_loc(h);  }
+    anki_s_change_lane(h,-50,100,1000);
+    for(i=0; i<10; i++){ usleep(200000);  print_loc(h);  }
+    anki_s_set_speed(h,0,20000);
+    sleep(1);
+
+    // Disconnect
+    anki_s_close(h);
+    return 0;
 }
