dbb0cc8fe111ea8a31ce86a94d0c363a438a2ce7
ameroyer <ameroyer@users.noreply.github.com>
2015-11-12 16:37:12 +0100

    merge failed

--- main.cpp
+++ main.cpp
@@ -109,7 +109,7 @@ void* update_camera_loc(void* aux) {
 
     //Init structures
     init_blob_detector();
-    init_states_list("CV/centroids_h100_v3");
+    //init_states_list("CV/centroids_h100_v3");
     camera_obst = (camera_obst_localization_t*) malloc(sizeof(camera_obst_localization_t));
     camera_obst->total = args->n_obst;
     camera_loc = (camera_localization_t*) malloc(sizeof(camera_localization_t));
